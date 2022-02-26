82805111c48faeba5ea4b133426778ac677f411d
shamanth.gs <shamanth.gs@gmail.com>
2022-02-26 07:09:36 -0700

    conditional styling

---
+++ src/Attempt.js
@@ -0,0 +1,112 @@
+import TableCell from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
+import { useState, useCallback } from 'react';
+import { ButtonGroup, Button } from '@mui/material';
+import { Done, Clear } from '@mui/icons-material';
+import { makeStyles } from '@mui/styles';
+import classNames from 'classnames';
+
+const useStyles = makeStyles((theme) => {
+  const commonLetterInput = {
+    width: '20px',
+    height: '20px',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    outline: 'none',
+  };
+  return {
+    letterInput: { ...commonLetterInput },
+    partialMatch: {
+      backgroundColor: 'yellow',
+    },
+    perfectMatch: {
+      backgroundColor: 'green',
+    },
+    noMatch: {
+      backgroundColor: 'white',
+    },
+  };
+});
+
+const deleteActions = ['Delete', 'Backspace'];
+const PERFECT_MATCH = 0;
+const PARTIAL_MATCH = 1;
+const NO_MATCH = 2;
+
+const validateLetter = (word, letter, letterIndex) => {
+  if (!word.includes(letter)) {
+    return NO_MATCH;
+  }
+  if (word.indexOf(letter) === letterIndex) {
+    return PERFECT_MATCH;
+  }
+  return PARTIAL_MATCH;
+};
+
+export const Attempt = ({ correctWord, attempt, numberOfLetters }) => {
+  const [word, setWord] = useState('');
+  const [matches, setMatches] = useState('');
+  const classes = useStyles();
+
+  const handleKeyDown = useCallback(
+    (keyPressEvent) => {
+      if (keyPressEvent.key.length === 1 && word.length < numberOfLetters) {
+        setWord((word) => word + keyPressEvent.key.toLocaleUpperCase());
+      }
+      if (deleteActions.includes(keyPressEvent.key)) {
+        setWord((word) => word.slice(0, -1));
+      }
+    },
+    [setWord, word.length, numberOfLetters]
+  );
+
+  const handleClear = useCallback(() => {
+    setWord('');
+  }, [setWord]);
+
+  const handleSubmit = useCallback(() => {
+    const matches = Array.from(word)
+      .map((letter, index) => validateLetter(correctWord, letter, index))
+      .join('');
+    setMatches(matches);
+    console.log('correctWord', correctWord);
+    console.log('word', word);
+    console.log('matches', matches);
+  }, [setMatches, word, correctWord]);
+
+  return (
+    <TableRow key={attempt}>
+      {[...Array.from({ length: numberOfLetters }).keys()].map(
+        (letterIndex) => (
+          <TableCell key={letterIndex} align="center" size="small">
+            <input
+              className={classNames({
+                [classes.letterInput]: true,
+                [classes.partialMatch]:
+                  parseInt(matches[letterIndex]) === PARTIAL_MATCH,
+                [classes.perfectMatch]:
+                  parseInt(matches[letterIndex]) === PERFECT_MATCH,
+                [classes.noMatch]: parseInt(matches[letterIndex]) === NO_MATCH,
+              })}
+              type="text"
+              value={word.length > letterIndex ? word[letterIndex] : ''}
+              onKeyDown={handleKeyDown}
+              maxLength={1}
+              readOnly
+            />
+          </TableCell>
+        )
+      )}
+      <TableCell key="actions" size="small" align="center">
+        <ButtonGroup aria-label="actions">
+          <Button size="small" aria-label="submit" onClick={handleSubmit}>
+            <Done />
+          </Button>
+          <Button size="small" aria-label="clear" onClick={handleClear}>
+            <Clear />
+          </Button>
+        </ButtonGroup>
+      </TableCell>
+    </TableRow>
+  );
+};
--- src/Wordle.js
+++ src/Wordle.js
@@ -1,4 +1,3 @@
-import { useState, useCallback } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -6,89 +5,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { ButtonGroup, Button } from '@mui/material';
-import { Done, Clear } from '@mui/icons-material';
-
-const deleteActions = ['Delete', 'Backspace'];
-const PERFECT_MATCH = 0;
-const PARTIAL_MATCH = 1;
-const NO_MATCH = 2;
-
-const validateLetter = (word, letter, letterIndex) => {
-  if (!word.includes(letter)) {
-    return NO_MATCH;
-  }
-  if (word.indexOf(letter) === letterIndex) {
-    return PERFECT_MATCH;
-  }
-  return PARTIAL_MATCH;
-};
-
-const Attempt = ({ correctWord, attempt, numberOfLetters }) => {
-  const [word, setWord] = useState('');
-  const [matches, setMatches] = useState('');
-
-  const handleKeyDown = useCallback(
-    (keyPressEvent) => {
-      if (keyPressEvent.key.length === 1 && word.length < numberOfLetters) {
-        setWord((word) => word + keyPressEvent.key.toLocaleUpperCase());
-      }
-      if (deleteActions.includes(keyPressEvent.key)) {
-        setWord((word) => word.slice(0, -1));
-      }
-    },
-    [setWord, word.length, numberOfLetters]
-  );
-
-  const handleClear = useCallback(() => {
-    setWord('');
-  }, [setWord]);
-
-  const handleSubmit = useCallback(() => {
-    const matches = Array.from(word)
-      .map((letter, index) => validateLetter(correctWord, letter, index))
-      .join('');
-    setMatches(matches);
-    console.log('correctWord', correctWord);
-    console.log('word', word);
-    console.log('matches', matches);
-  }, [setMatches, word, correctWord]);
-
-  return (
-    <TableRow key={attempt}>
-      {[...Array.from({ length: numberOfLetters }).keys()].map(
-        (letterIndex) => (
-          <TableCell key={letterIndex} align="center" size="small">
-            <input
-              style={{
-                width: '20px',
-                height: '20px',
-                fontWeight: 'bold',
-                textAlign: 'center',
-                outline: 'none',
-              }}
-              type="text"
-              value={word.length > letterIndex ? word[letterIndex] : ''}
-              onKeyDown={handleKeyDown}
-              maxLength={1}
-              readOnly
-            />
-          </TableCell>
-        )
-      )}
-      <TableCell key="actions" size="small" align="center">
-        <ButtonGroup aria-label="actions">
-          <Button size="small" aria-label="submit" onClick={handleSubmit}>
-            <Done />
-          </Button>
-          <Button size="small" aria-label="clear" onClick={handleClear}>
-            <Clear />
-          </Button>
-        </ButtonGroup>
-      </TableCell>
-    </TableRow>
-  );
-};
+import { Attempt } from './Attempt';
 
 export const BasicTable = ({ correctWord }) => {
   const numberOfLetters = correctWord.length;
